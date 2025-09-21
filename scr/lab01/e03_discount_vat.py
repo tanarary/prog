40cb08827cb9b999a85a57683d2270b9b2446d46
Соня <sona@MacBook-Pro-Ksenia.local>
2025-09-21 22:13:45 +0300

    1

---
+++ scr/lab01/e03_discount_vat.py
@@ -0,0 +1,7 @@
+price = float(input('Введите цену: '))
+sale = float(input('Введите скидку: '))
+vat =  float(input('Введите НДС: '))
+base = price * (1 - sale/100)
+print(f"""База после скидки: {sale} ₽
+НДС:               {base * (vat/100)} ₽
+Итого к оплате:    {base * (vat/100) + base} ₽""")
