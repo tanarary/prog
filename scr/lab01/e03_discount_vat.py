d1d07dfbe2639ce6883dab4cabb30ce1a1cc353b
Sonya <kazantseva.s@icloud.com>
2025-09-25 00:20:43 +0300

    222

--- scr/lab01/e03_discount_vat.py
+++ scr/lab01/e03_discount_vat.py
@@ -2,6 +2,6 @@ price = float(input('Введите цену: '))
 sale = float(input('Введите скидку: '))
 vat =  float(input('Введите НДС: '))
 base = price * (1 - sale/100)
-print(f"""База после скидки: {sale} ₽
+print(f"""База после скидки: {base} ₽
 НДС:               {base * (vat/100)} ₽
 Итого к оплате:    {base * (vat/100) + base} ₽""")
